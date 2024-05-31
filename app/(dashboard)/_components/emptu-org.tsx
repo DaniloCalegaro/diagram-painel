1048e38587d5f3c7825d264916b44131e79072cf
DaniloCalegaro <danilo.calegaro@hotmail.com>
2024-05-31 16:12:45 -0300

    ➕Feat: DashboardPage Empty Org

---
+++ app/(dashboard)/_components/emptu-org.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { CreateOrganization } from "@clerk/nextjs";
+import Image from "next/image";
+
+export const EmptyOrg = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src={"/elements.svg"} alt="Empty" width={200} height={200} />
+      <h2 className="text-2xl font-semibold mt-6">Welcome to Board</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Create an organization to get started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create organization</Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-none max-w-[432px]">
+            <CreateOrganization routing="virtual" />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,27 @@
-const DashboardPage = () => {
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/emptu-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <div>Dashboard Root Page</div>
+    <div className="flex-1 h-[calc(100%-80px)]">
+      {JSON.stringify(searchParams)}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
